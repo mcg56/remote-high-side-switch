a2d41dbb324ff5541841cf76a8434ed96250d6a2
mcg56 <94398601+mcg56@users.noreply.github.com>
2023-05-22 15:40:04 +1200

    Add files via upload

---
+++ final.ino
@@ -0,0 +1,249 @@
+#include <Arduino.h>
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
+
+#include <iostream>
+#include <sstream>
+
+#define UP 1
+#define DOWN 2
+#define STOP 0
+#define STATUS_LED  10
+#define UP_PIN      8
+#define DOWN_PIN    3
+
+#define RIGHT_MOTOR 0
+#define LEFT_MOTOR 1
+
+const char* ssid     = "Trailer Controller";
+const char* password = "12345678";
+
+AsyncWebServer server(80);
+AsyncWebSocket wsCarInput("/CarInput");
+
+const char* htmlHomePage PROGMEM = R"HTMLHOMEPAGE(
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <style>
+    .arrows {
+      font-size:40px;
+      color:red;
+    }
+    td.button {
+      background-color:black;
+      border-radius:10%;
+      box-shadow: 5px 5px #888888;
+    }
+    td.button:active {
+      transform: translate(5px,5px);
+      box-shadow: none; 
+    }
+
+    .noselect {
+      -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+         -khtml-user-select: none; /* Konqueror HTML */
+           -moz-user-select: none; /* Firefox */
+            -ms-user-select: none; /* Internet Explorer/Edge */
+                user-select: none; /* Non-prefixed version, currently
+                                      supported by Chrome and Opera */
+    }
+
+    
+
+    </style>
+  
+  </head>
+  <body class="noselect" align="center" style="background-color:white">
+     
+    <h1 style="color: black;text-align:center;font-family:verdana;">Hydraulic Trailer</h1>
+    <h2 style="color: black;text-align:center;font-family:verdana;">WiFi Control</h2>
+    
+    <table id="mainTable" style="width:400px;margin:auto;table-layout:fixed" CELLSPACING=10>
+      <tr>
+        <td></td>
+        <td class="button" ontouchstart='sendButtonInput("MoveCar","1")' ontouchend='sendButtonInput("MoveCar","0")'><span class="arrows" >&#8679;</span></td>
+        <td></td>
+      </tr>
+      <tr>
+      </tr>
+      <tr>
+        <td></td>
+        <td class="button" ontouchstart='sendButtonInput("MoveCar","2")' ontouchend='sendButtonInput("MoveCar","0")'><span class="arrows" >&#8681;</span></td>
+        <td></td>
+      </tr>
+      <tr/><tr/>
+      <tr/><tr/>
+      <tr/><tr/>      
+    </table>
+
+    <h3 style="color: black;text-align:center;font-family:verdana;">Rev 1.0 - 2/04/2023</h3>
+  
+    <script>
+      var webSocketCarInputUrl = "ws:\/\/" + window.location.hostname + "/CarInput";      
+      var websocketCarInput;
+      
+      function initCarInputWebSocket() 
+      {
+        websocketCarInput = new WebSocket(webSocketCarInputUrl);
+        websocketCarInput.onopen    = function(event)
+        {
+          var speedButton = document.getElementById("Speed");
+          sendButtonInput("Speed", speedButton.value);
+        };
+        websocketCarInput.onclose   = function(event){setTimeout(initCarInputWebSocket, 2000);};
+        websocketCarInput.onmessage = function(event){};        
+      }
+      
+      function sendButtonInput(key, value) 
+      {
+        var data = key + "," + value;
+        websocketCarInput.send(data);
+      }
+    
+      window.onload = initCarInputWebSocket;
+      document.getElementById("mainTable").addEventListener("touchend", function(event){
+        event.preventDefault()
+      });      
+    </script>
+  </body>    
+</html>
+)HTMLHOMEPAGE";
+
+
+void rotateMotor(int motorNumber, int motorDirection)
+{
+}
+
+void moveCar(int inputValue)
+{
+  Serial.printf("Got value as %d\n", inputValue);  
+  switch(inputValue)
+  {
+
+    case UP:
+      digitalWrite(STATUS_LED, LOW);    
+      digitalWrite(UP_PIN, HIGH); 
+      digitalWrite(DOWN_PIN, LOW);     
+      break;
+  
+    case DOWN:
+      digitalWrite(STATUS_LED, LOW); 
+      digitalWrite(UP_PIN, LOW);
+      digitalWrite(DOWN_PIN, HIGH);        
+      break;
+ 
+    case STOP:
+      digitalWrite(STATUS_LED, HIGH);
+      digitalWrite(UP_PIN, LOW);   
+      digitalWrite(DOWN_PIN, LOW);
+      break;
+  
+    default:
+      digitalWrite(STATUS_LED, HIGH); 
+      digitalWrite(UP_PIN, LOW);    
+      digitalWrite(DOWN_PIN, LOW);
+      break;
+  }
+}
+
+void handleRoot(AsyncWebServerRequest *request) 
+{
+  request->send_P(200, "text/html", htmlHomePage);
+}
+
+void handleNotFound(AsyncWebServerRequest *request) 
+{
+    request->send(404, "text/plain", "File Not Found");
+}
+
+void onCarInputWebSocketEvent(AsyncWebSocket *server, 
+                      AsyncWebSocketClient *client, 
+                      AwsEventType type,
+                      void *arg, 
+                      uint8_t *data, 
+                      size_t len) 
+{                      
+  switch (type) 
+  {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      moveCar(STOP);
+      break;
+    case WS_EVT_DATA:
+      AwsFrameInfo *info;
+      info = (AwsFrameInfo*)arg;
+      if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) 
+      {
+        std::string myData = "";
+        myData.assign((char *)data, len);
+        std::istringstream ss(myData);
+        std::string key, value;
+        std::getline(ss, key, ',');
+        std::getline(ss, value, ',');
+        Serial.printf("Key [%s] Value[%s]\n", key.c_str(), value.c_str()); 
+        int valueInt = atoi(value.c_str());     
+        if (key == "MoveCar")
+        {
+          moveCar(valueInt);        
+        }
+        else if (key == "Speed")
+        {
+          
+        }
+      }
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+    default:
+      break;  
+  }
+}
+
+void setUpPinModes()
+{
+  pinMode(STATUS_LED, OUTPUT);
+  pinMode(UP_PIN, OUTPUT);
+  pinMode(DOWN_PIN, OUTPUT);
+  digitalWrite(STATUS_LED, HIGH);
+  moveCar(STOP);
+}
+
+
+void setup(void) 
+{
+  setUpPinModes();
+  Serial.begin(115200);
+
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.on("/", HTTP_GET, handleRoot);
+  server.onNotFound(handleNotFound);
+      
+  wsCarInput.onEvent(onCarInputWebSocketEvent);
+  server.addHandler(&wsCarInput);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+}
+
+void loop() 
+{
+  wsCarInput.cleanupClients(); 
+}
